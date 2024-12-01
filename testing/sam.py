9af588b4e68088f6bc20ec868312fbad757d570e
Pavan <pavangiri510@gmail.com>
2024-12-01 22:55:57 +0530

    Added python 3 simple questions and answers

---
+++ testing/sam.py
@@ -0,0 +1,52 @@
+'''Question 1: Write a program that takes two numbers as input from the user and prints their sum, difference, product, and quotient.'''
+
+# num1 = int(input("Enter 1st Number:"))
+# num2 = int(input("Enter 2nd Number:"))
+
+# print("sum of 2 numbers", num1+num2)
+# print("diff of 2 numbers", num1-num2)
+# print("prod of 2 numbers", num1*num2)
+# print("div of 2 numbers", num1/num2)
+
+'''Question 2: Write a program that takes any value as input and prints its data type (e.g., integer, float, string, boolean).'''
+
+# val = input("Enter your value to know DataType:")
+# if val.isdigit():
+#     print("Datatype of your value is Integer")
+# elif '.' in val:
+#     print("Datatype of your value is Float")
+# elif isinstance(val, bool):
+#     print("Datatype of your value is Boolean")
+# else:
+#     print("Datatype of your value is String")
+
+
+'''Question 3: Take a float number from the user, cast it to an integer, and print both the original and the casted value.'''
+
+# f_num = float(input("Enter the float number:"))
+# print("Original float value:", f_num)
+# print("Casting float into integer value:", int(f_num))
+
+'''Question 4: Write a program that compares two numbers and prints True if the first number is greater, otherwise prints False.'''
+
+
+num1 = int(input("Enter 1st Number:"))
+num2 = int(input("Enter 2nd Number:"))
+
+if num1 > num2:
+    print("True")
+else:
+    print("False")
+
+"""
+What is a Python virtual environment, and why is it important?
+What are the key differences between lists, tuples, sets, and dictionaries in Python?
+Discuss the characteristics (mutability, indexing, ordering, etc.) of each data structure.
+What are string slicing and string formatting in Python? Provide examples.
+Explain how string slicing works and how to extract substrings from a string using indices.
+ HowdoesSciPy extend NumPy for scientific computing? Mention at least two features provided by SciPy.
+Discuss the role of SciPy in advanced mathematical, scientific, and engineering tasks, and mention features such as optimization, integration, and solving differential equations.
+What is the Seaborn library used for, and how does it differ from Matplotlib?
+Explain the advantages of Seaborn for creating statistical plots and discuss how it simplifies the process of creating complex visualizations like heatmaps, pair plots, and violin plots.
+Explain inheritance in Python and provide an example of how a child class can inherit properties and methods from a parent class.
+Discuss the concept of inheritance and the ability to extend and modify the behavior of parent classes. Provide a simple example of a base class and a derived class    """
